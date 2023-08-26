c0c14c75c96aef112d80554c73fe16bf10ff8ca2
Manil Singha Malla <manilsmalla@gmail.com>
2023-08-26 10:39:37 +0545

    FEAT: Create SearchBar component.

--- src/screens/ProductSearch.js
+++ src/screens/ProductSearch.js
@@ -1,10 +1,27 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useRef, useState } from "react";
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 const ProductSearch = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+    const inputRef = useRef(null);
+
+    const handleClear = () => {
+        inputRef.current.clear();
+    };
+
     return (
         <View style={styles.root}>
-            <Text>Product Search</Text>
+            <View style={styles.searchBar}>
+                <TextInput
+                    ref={inputRef}
+                    onChangeText={setSearchTerm}
+                    value={searchTerm}
+                    style={styles.input}
+                />
+                <TouchableOpacity onPress={handleClear} style={styles.clearButton}>
+                    <Text>Clear</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
@@ -13,6 +30,20 @@ const styles = StyleSheet.create({
     root: {
         justifyContent: "space-between",
         padding: 20,
+    },
+
+    searchBar: {
+        alignItems: 'center',
+        borderRadius: 10,
+        borderWidth: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 16,
+    },
+    input: { flex: 0.8 },
+    clearButton: {
+        alignItems: 'flex-end',
+        flex: 0.2,
     },
 });
 
